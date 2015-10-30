0f5f468899226249081b8eaf68ca11c086a1eb37
alu0100826166 <alu0100826166@ull.edu.es>
2015-10-30 11:11:00 +0000

    C5: Falla la tercera prueba

--- spec/spec_bibliografia.rb
+++ spec/spec_bibliografia.rb
@@ -2,6 +2,13 @@ require 'lib/bibliografia.rb'
 
 describe Bibliografia do
     before :each do
-        @b1=Bibliografia.new()
+        @b1=Bibliografia.new('Dave Thomas')
     end
+    context "#Autores" do
+        it "Debe existir uno o mas autores" do
+            @b1.autores.count.should >= 1
+        end
+    end
+        
+    
 end
