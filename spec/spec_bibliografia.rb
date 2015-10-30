f6090e95a3a9787753917ee8bc8f2caf16459fab
alu0100826166 <alu0100826166@ull.edu.es>
2015-10-30 15:19:52 +0000

    C7: Falla la cuarta prueba

--- spec/spec_bibliografia.rb
+++ spec/spec_bibliografia.rb
@@ -4,11 +4,16 @@ require 'bibliografia'
 
 describe Bibliografia do
     before :each do
-        @b1 = Bibliografia::Bibliografia.new("Nombre Autor") #Clase Bibliografia del MODULO Bibliografia!!!!!!!
+        @b1 = Bibliografia::Bibliografia.new("Nombre Autor", "Titulo") #Clase Bibliografia del MODULO Bibliografia!!!!!!!
     end #before each
     context "#Autores" do
         it "Debe existir uno o mas autores" do
           @b1.autores.count.should >= 1
+        end
+    end #context
+    context "#Titulo" do
+        it "Debe existir un titulo" do
+          @b1.titulo.count.should >= 1
         end
     end #context
         
