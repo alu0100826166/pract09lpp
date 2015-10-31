90749900a824368a73f1502ff193ffb0aa04c21e
Alejandro Marrero Díaz <alexmd54@gmail.com>
2015-10-31 18:06:10 +0000

    C18 prueba ISBN pasada

--- lib/bibliografia/bibliografia.rb
+++ lib/bibliografia/bibliografia.rb
@@ -3,13 +3,16 @@ module Bibliografia
     
     class Bibliografia
 
-        attr_accessor :autores, :titulo, :editorial, :fecha, :n_edicion
+        attr_accessor :autores, :titulo, :editorial, :fecha, :n_edicion, :isbn
 
-        def initialize(autor, titulo, ed, nEd, fecha)
+        def initialize(autor, titulo, ed, nEd, fecha, isbn)
             raise ArgumentError, 'Tiene que haber al menos un autor' if autor.length == 0
             raise ArgumentError if ( (fecha.class != Date) ||
                                     !(Date.valid_date?(fecha.year,fecha.month, fecha.mday)))
+            
+            raise ArgumentError if (isbn.class != Hash)
             @autores = Array.new
+            @isbn = isbn
             @titulo = titulo
             @editorial = ed
             @fecha = fecha
