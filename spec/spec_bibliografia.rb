67305612a8cc83f891f8d4c6b2f099353b88b597
alu0100826166 <alu0100826166@ull.edu.es>
2015-11-17 11:42:35 +0000

    C1: Falla 1 prueba

--- spec/spec_bibliografia.rb
+++ spec/spec_bibliografia.rb
@@ -4,20 +4,23 @@ require 'bibliografia'
 
 describe Bibliografia do
     before :all do
-        serie1 = "Serie"
-        nombres = Array.new
-        nombres = %w{ Autor1 Autor2 }
-        isbn = { "isbn-10" => " 1937785491", "isbn-13" => " 978-1937785499" }
-        @b1 = Bibliografia::Bibliografia.new(nombres, "Titulo", serie1, "Editorial", "Numero de edicion 4", Date.new(2015,10,31), isbn) #Clase Bibliografia del MODULO Bibliografia!!!!!!!
+        # serie1 = "Serie"
+        # nombres = Array.new
+        # nombres = %w{ Autor1 Autor2 }
+        # isbn = { "isbn-10" => " 1937785491", "isbn-13" => " 978-1937785499" }
+        # @b1 = Bibliografia::Bibliografia.new(nombres, "Titulo", serie1, "Editorial", "Numero de edicion 4", Date.new(2015,10,31), isbn) #Clase Bibliografia del MODULO Bibliografia!!!!!!!
         
-        serie2 = "Serie1"
-        nombres1 = Array.new
-        nombres1 = %w{ DaveThomas AndyHunt ChadFowler }
-        isbn1 = { "isbn-1" => " 193778", "isbn-12" => " 978-19377" }
-        @b2 = Bibliografia::Bibliografia.new(nombres1, "Titulo1", serie2, "Editorial1", "Numero de edicion 1", Date.new(2015,11, 10), isbn1)
+        # serie2 = "Serie1"
+        # nombres1 = Array.new
+        # nombres1 = %w{ DaveThomas AndyHunt ChadFowler }
+        # isbn1 = { "isbn-1" => " 193778", "isbn-12" => " 978-19377" }
+        # @b2 = Bibliografia::Bibliografia.new(nombres1, "Titulo1", serie2, "Editorial1", "Numero de edicion 1", Date.new(2015,11, 10), isbn1)
         
-        @nodo = Bibliografia::Node.new(@b1, nil) #Clase Node
-        @lista= Bibliografia::ListaEnlazada.new(@b2)
+        # @nodo = Bibliografia::Node.new(@b1, nil) #Clase Node
+        # @lista= Bibliografia::ListaEnlazada.new(@b2)
+        
+        
+        @libro = Bibliografia::Libro.new()
     end #before each
     
 #     context "#Autores" do
@@ -85,34 +88,41 @@ describe Bibliografia do
     
 
 ########################################################## PRUEBAS PRACTICA 7
-    context "#Debe existir un nodo" do
-        it "Debe existir un nodo con un valor" do
-            @nodo.value.should eq(@b1)
-        end
-        it "Debe existir un nodo con un siguiente" do
-            @nodo.sig.should eq(nil)
-        end
-    end
+    # context "#Debe existir un nodo" do
+    #     it "Debe existir un nodo con un valor" do
+    #         @nodo.value.should eq(@b1)
+    #     end
+    #     it "Debe existir un nodo con un siguiente" do
+    #         @nodo.sig.should eq(nil)
+    #     end
+    # end
     
-    context "#Debe existir una lista enlazada" do
-        it "Debe existir una lista" do
-            @lista.head.value.should eq(@b2)
-        end
+    # context "#Debe existir una lista enlazada" do
+    #     it "Debe existir una lista" do
+    #         @lista.head.value.should eq(@b2)
+    #     end
         
-        it "Debe insertar un elemento" do
-            @lista.push(@b2)
-        end
+    #     it "Debe insertar un elemento" do
+    #         @lista.push(@b2)
+    #     end
         
-        it "Debe insertar un elemento" do
-            @lista.push(@b1)
-        end
+    #     it "Debe insertar un elemento" do
+    #         @lista.push(@b1)
+    #     end
         
-        # it "Debe extraerse el primer elemento" do
-        #     @lista.pop
-        # end
+    #     # it "Debe extraerse el primer elemento" do
+    #     #     @lista.pop
+    #     # end
         
-        it "Debe extraer todos los elementos de la lista" do
-            @lista.extraer
+    #     it "Debe extraer todos los elementos de la lista" do
+    #         @lista.extraer
+    #     end
+    # end
+    
+########################################################PRUEBAS PRACTICA 8
+    context "#Debe existir una clase libro" do
+        it "Debe existir una clase libro" do
+            @libro.should.eq (@libro)
         end
     end
 end
