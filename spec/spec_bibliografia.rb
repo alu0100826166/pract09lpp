6bcfc68b94e4a91eb7fce555adb1b2a41562f14c
alu0100826166 <alu0100826166@ull.edu.es>
2015-11-26 13:17:33 +0000

    C5: Falla prueba minimo

--- spec/spec_bibliografia.rb
+++ spec/spec_bibliografia.rb
@@ -82,6 +82,34 @@ describe Bibliografia do
             expect(@libro > @libro2).to eq(false)
         end
     end
+    
+    context "Debe haber un modulo enumerable para clase lista" do
+        it "Debe sacarse el minimo" do
+            series = "Serie1"
+            autores22 = Array.new
+            autores22 = %w{ Autor1 Autor2 }
+            isbns = { "isbn-1" => " 193778", "isbn-12" => " 978-19377" }
+            @lib = Bibliografia::Libro.new(autores22, "TituloLibro", series, "Editorial Libro", "Numero Edicion Libro", Date.new(2015, 11, 17), isbns)
+                
+            serie2s = "Serie2"
+            autores11 = Array.new
+            autores11 = %w{ Autor1 Autor2 }
+            issns = "1133-9721"
+            @rev = Bibliografia::Revista.new(autores11, "TituloRevista", serie2s, "Editorial Revista", "Numero Edicion Revista", Date.new(2015, 11, 17), issns)
+                
+            autores33 = Array.new
+            autores33 = %w{ Autor1 Autor2 }
+            urls = "www.libroelectronico.com"
+            @docu = Bibliografia::DocumentoElectronico.new(autores33, "TituloDoc", Date.new(2015, 11, 17), urls)
+            
+            @list=Bibliografia::ListaEnlazada.new(@lib)
+            @list.insertar_delante(@rev)
+            @list.insertar_detras(@docu)
+            
+            @list.min
+       
+        end
+    end
 
 
 end
